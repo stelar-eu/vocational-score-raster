fc1788eb3caf711e62d59963e8b5e6c38f998f92
Dimitris Petrou <petroudimitris@outlook.com>
2025-01-23 20:08:09 +0200

    Update run.sh with newly added signature validation parameter for API interaction

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if three arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <token> <endpoint_url> <id>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <token> <endpoint_url> <id> <signature>"
     exit 2
 fi
 
@@ -10,9 +10,12 @@ fi
 token="$1"
 endpoint_url="$2"
 id="$3"
+signature="$4"
 
 # Construct the first curl command with dynamic arguments and store the response in a variable
-response=$(curl -s -L -X GET -H "Authorization: $token" "$endpoint_url/api/v2/tasks/$id/input")
+# Signature is used to gain access to any secret fields defined during the creation of the task.
+# Secret fields are not accessible without a valid signature.
+response=$(curl -s -L -X GET -H "Authorization: $token" "$endpoint_url/api/v2/tasks/$id/$signature/input")
 
 echo "$response"
 
@@ -37,8 +40,9 @@ output_json=$(<output.json)  # Read content of output.json file into a variable
 
 echo "$output_json"
 
-# Replace task_exec_id and output_json with variables
-response=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: $token" "$endpoint_url/api/v2/tasks/$id/output" -d "{\"task_exec_id\": \"$id\", \"output_json\": $output_json}")
+# Replace task_exec_id and output_json with variables. The signature is used to verify that the tool is authenticated.
+# We will not be using token, as it may expire prior to tool completion.
+response=$(curl -s -X POST -H "Content-Type: application/json" "$endpoint_url/api/v2/tasks/$id/output" -d "{\"signature\": \"$signature\" \"outputs\": $output_json}")
 
 echo "$response"
 
